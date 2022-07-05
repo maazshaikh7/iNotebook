e6ada14a281385dd33078800fce11012ad5aa5cd
Maaz <maazshaikh4559@gmail.com>
2022-07-05 23:04:22 +0530

    "One day"

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -5,17 +5,18 @@ const fetchuser = (req, res, next) => {
     // Get the user from the jwt token and add id to req object
     const token = req.header('auth-token');
     if (!token) {
-        res.status(401).send({ error: "Please authenticate using a valid token" })
+        res.status(401).send({ error: "Ends up here with auth-token heading unchecked" })
     }
     try {
         user = {
             id: user.id
         }
         const data = jwt.verify(token, JWT_SECRET);
-        req.user = data.user;
+        const id = data.iat
+        req.user = id;
         next();
     } catch (error) {
-        res.status(401).send({ error: "Please authenticate using a valid token", error })
+        res.status(401).send({ error: "Always ends up here somehow"})
     }
 
 }
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const fetchuser = require('../middleware/fetchuser');
-const Note = require('../models/Notes');
+const Note = require('../models/Note');
 const { body, validationResult } = require('express-validator');
 
 // ROUTE 1: Get All the Notes using: GET "/api/notes/getuser". Login required
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
     try {
-        user = {
-            id: user.id
-        }
         const notes = await Note.find({ user: req.user.id });
         res.json(notes)
     } catch (error) {
